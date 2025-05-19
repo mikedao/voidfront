9d4652abb817bb8442da5be35f9b6366ad6fb76d
Michael Dao <mike@turing.edu>
2025-05-18 21:53:41 -0400

    Enhance ScheduleMaintenanceJob to handle errors during maintenance scheduling for each empire, logging failures for better traceability.

--- app/jobs/schedule_maintenance_job.rb
+++ app/jobs/schedule_maintenance_job.rb
@@ -2,8 +2,12 @@ class ScheduleMaintenanceJob < ApplicationJob
   queue_as :default
 
   def perform
-    Empire.pluck(:id).each do |empire_id|
-      MaintenanceJob.perform_later(empire_id)
+    Empire.find_each(batch_size: 100) do |empire|
+      begin
+        MaintenanceJob.perform_later(empire.id)
+      rescue StandardError => e
+        Rails.logger.error("Failed to schedule maintenance for Empire #{empire.id}: #{e.message}")
+      end
     end
   end
 end
