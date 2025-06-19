431eabd5d0440d5eb64c43e7104ffe1f902acb23
Michael Dao <mike@turing.edu>
2025-06-18 20:05:59 -0400

    Update Ruby version to 3.4.4 and upgrade dependencies in Gemfile and Gemfile.lock; enhance MaintenanceJob to manage building statuses during maintenance tasks with corresponding tests.

--- app/jobs/maintenance_job.rb
+++ app/jobs/maintenance_job.rb
@@ -12,11 +12,30 @@ class MaintenanceJob < ApplicationJob
 
   def maintenance_tasks
     ActiveRecord::Base.transaction do
+      # handle building status updates
+      update_building_statuses
+
+      # collect tax revenue
       @empire.update(credits: @empire.credits + tax_revenue) if tax_revenue > 0
 
+      # update population
       @empire.star_systems.each do |system|
         system.update(current_population: system.new_population)
       end
+    end
+  end
+
+  def update_building_statuses
+    @empire.star_systems.each do |system|
+      # Find buildings where construction has finished
+      system.buildings.where(status: "under_construction")
+        .where("construction_end <= ?", Time.current)
+        .update_all(status: "operational")
+
+      # Remove buildings where demolition has finished
+      system.buildings.where(status: "being_demolished")
+        .where("demolition_end <= ?", Time.current)
+        .destroy_all
     end
   end
 
--- spec/jobs/maintenance_job_spec.rb
+++ spec/jobs/maintenance_job_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe MaintenanceJob, type: :job do
   let!(:empire) { create(:empire, user: user, credits: 1000, tax_rate: 10) }
   let!(:star_system) { create(:star_system, empire: empire, current_population: 500) }
   let!(:star_system_2) { create(:star_system, empire: empire, current_population: 300) }
+  let!(:building_type) { create(:building_type) }
 
   describe "#perform" do
     it 'collects taxes based on population and tax rate' do
@@ -33,6 +34,54 @@ RSpec.describe MaintenanceJob, type: :job do
       expect {
         MaintenanceJob.perform_now(empire.id)
       }.to change { system.reload.current_population }
+    end
+  end
+
+  describe "handling building status updates" do
+    let(:user) { create(:user) }
+    let(:empire) { create(:empire, user: user) }
+    let(:star_system) { create(:star_system, empire: empire) }
+    let(:building_type) { create(:building_type) }
+
+    it "completes buildings whose construction time has passed" do
+      building = create(:building, 
+                        star_system: star_system, 
+                        building_type: building_type, 
+                        construction_end: 1.minute.ago)
+
+      MaintenanceJob.perform_now(empire.id)
+
+      expect(building.reload.status).to eq("operational")
+    end
+
+    it "doesn't complete buildings that haven't finished construction" do
+      building = create(:building, :under_construction,
+                        star_system: star_system, 
+                        building_type: building_type, 
+                        construction_end: 1.hour.from_now)
+
+      MaintenanceJob.perform_now(empire.id)
+
+      expect(building.reload.status).to eq("under_construction")
+    end
+
+    it "removes buildings that have been demolished" do
+      building = create(:building, :being_demolished,
+                        star_system: star_system,
+                        building_type: building_type,
+                        demolition_end: 1.minute.ago)
+
+      expect { MaintenanceJob.perform_now(empire.id) }.to change { Building.count }.by(-1)
+    end
+
+    it "doesn't remove buildings still being demolished" do
+      building = create(:building, :being_demolished,
+                        star_system: star_system,
+                        building_type: building_type,
+                        demolition_end: 1.hour.from_now)
+
+      expect { MaintenanceJob.perform_now(empire.id) }.not_to change { Building.count }
+      expect(building.reload.status).to eq("being_demolished")
     end
   end
 end
